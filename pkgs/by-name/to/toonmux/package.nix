b90b4b39815c13e13a6b587e78c6c414001f3d1a
powwu <hello@powwu.sh>
2026-02-17 11:00:45 -0500

    toonmux init 0.0.9

---
+++ pkgs/by-name/to/toonmux/package.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+  makeDesktopItem,
+  pkg-config,
+  gtk3,
+  xdotool,
+}:
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "toonmux";
+  version = "0.0.9";
+
+  src = fetchFromGitHub {
+    owner = "JonathanHelianthicusDoe";
+    repo = "toonmux";
+    rev = "4571c53dbcb46c2e27adb94612ab8052dbe9a324";
+    hash = "sha256-GwpU/6JkCb11+FfgNUoFIXerwQSOexn/GlkESRzjKIA=";
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    gtk3
+    xdotool
+  ];
+
+  postPatch = ''
+    cp -L ${./Cargo.lock} Cargo.lock
+  '';
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = "toonmux";
+      desktopName = "toonmux Multicontroller";
+      genericName = "Toontown Multicontroller";
+      exec = "toonmux";
+    })
+  ];
+
+  meta = {
+    description = "Multi-toon controller for Toontown-based MMORPGs";
+    homepage = "https://github.com/JonathanHelianthicusDoe/toonmux";
+    license = lib.licenses.gpl3Plus;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ powwu ];
+  };
+})
