b4143a6adf73d9803dff72948dee588d15104b2d
powwu <hazy@vivaldi.net>
2026-08-26 14:17:05 -0400

    Update package.nix

--- pkgs/by-name/tu/tunetoon/package.nix
+++ pkgs/by-name/tu/tunetoon/package.nix
@@ -3,16 +3,84 @@
   stdenvNoCC,
   fetchurl,
   appimage-run,
+  buildFHSEnv,
   makeDesktopItem,
   copyDesktopItems,
   makeWrapper,
+
+  dotnet-runtime_10,
+  fontconfig,
+  libGL,
+  icu,
+  libx11,
+  libxext,
+  libxrandr,
+  libxi,
+  libxcursor,
+  libice,
+  libsm,
+  libxcomposite,
+  libxdamage,
+  libxfixes,
+  libxrender,
+  libxtst,
+  libxkbcommon,
+  dbus,
+  pulseaudio,
+  alsa-lib,
+  pipewire,
+  bottles,
+  winetricks,
+  cabextract,
+  xorgserver,
+  gamescope,
 }:
+
 let
   appimage = fetchurl {
     url = "https://github.com/the-finest-noobs/ToonSuite-releases/releases/download/v1.1.0/Tunetoon-Linux.AppImage";
     hash = "sha256-GeLL7i2VzepDvCZG6DL27IHzJSkRDXS6DMvpBl4iRgg=";
   };
+
+  fhs = buildFHSEnv {
+    name = "tunetoon-fhs";
+
+    targetPkgs = pkgs: [
+      dotnet-runtime_10
+      fontconfig
+      libGL
+      icu
+
+      libx11
+      libxext
+      libxrandr
+      libxi
+      libxcursor
+      libice
+      libsm
+      libxcomposite
+      libxdamage
+      libxfixes
+      libxrender
+      libxtst
+
+      libxkbcommon
+      dbus
+      pulseaudio
+      alsa-lib
+      pipewire
+
+      bottles
+      winetricks
+      cabextract
+      xorgserver
+      gamescope
+    ];
+
+    runScript = "${appimage-run}/bin/appimage-run";
+  };
 in
+
 stdenvNoCC.mkDerivation {
   pname = "tunetoon";
   version = "1.1.0";
@@ -27,9 +95,17 @@ stdenvNoCC.mkDerivation {
   ];
 
   installPhase = ''
-    install -Dm755 $src $out/share/tunetoon/Tunetoon-Linux.AppImage
-    makeWrapper ${appimage-run}/bin/appimage-run $out/bin/tunetoon \
-      --add-flags "$out/share/tunetoon/Tunetoon-Linux.AppImage"
+    runHook preInstall
+
+    install -Dm755 \
+      $src \
+      $out/share/tunetoon/Tunetoon-Linux.AppImage
+
+    makeWrapper ${fhs}/bin/tunetoon-fhs $out/bin/tunetoon \
+      --add-flags "$out/share/tunetoon/Tunetoon-Linux.AppImage" \
+      --set DOTNET_ROOT "${dotnet-runtime_10}/share/dotnet"
+
+    runHook postInstall
   '';
 
   desktopItems = [
@@ -38,7 +114,7 @@ stdenvNoCC.mkDerivation {
       desktopName = "Tunetoon";
       genericName = "Multi-toon Launcher";
       exec = "tunetoon";
-      categories = [ "Game" ];
+      categories = [ "Utility" ];
     })
   ];
 
