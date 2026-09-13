2a4c619872d82849dda6b82c1ca6b12da5940d5a
powwu <hello@powwu.sh>
2026-09-13 09:21:29 -0400

    .

--- pkgs/by-name/no/not-spotify/package.nix
+++ pkgs/by-name/no/not-spotify/package.nix
@@ -88,21 +88,21 @@
     pango
     stdenv.cc.cc
     systemd
-    xorg.libICE
-    xorg.libSM
-    xorg.libX11
-    xorg.libxcb
-    xorg.libXcomposite
-    xorg.libXcursor
-    xorg.libXdamage
-    xorg.libXext
-    xorg.libXfixes
-    xorg.libXi
-    xorg.libXrandr
-    xorg.libXrender
-    xorg.libXScrnSaver
-    xorg.libxshmfence
-    xorg.libXtst
+    libICE
+    libSM
+    libX11
+    libxcb
+    libXcomposite
+    libXcursor
+    libXdamage
+    libXext
+    libXfixes
+    libXi
+    libXrandr
+    libXrender
+    libXScrnSaver
+    libxshmfence
+    libXtst
     zlib
   ];
 in
